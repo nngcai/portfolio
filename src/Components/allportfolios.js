25e35e024ae75418140e5829ba4c83923ed95bc8
nngcai <nngcai@student.wethinkcode.co.za>
2021-05-21 10:00:49 +0200

    Added another Project

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -1,7 +1,7 @@
 import port1 from '../img/portImages/port2.jpg';
 import port2 from '../img/portImages/port1.jpg';
 import port4 from '../img/portImages/port3.jpg';
-// import port6 from '../img/portImages/port5.jpg';
+import port6 from '../img/portImages/port5.jpg';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -15,6 +15,15 @@ const portfolios = [
                 </Link>,
         image: port2,
         title: 'Python Code Clinics Command Line App'
+    },
+    {
+        id: 2,
+        category: 'ReactJS',
+        icon1: <Link to={{pathname: "https://github.com/nngcai/portfolio"}} target="_blank" className="icon-holder">
+                    <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                </Link>,
+        image: port6,
+        title: 'Building a portfolio website using ReactJS',
     },
     {
         id: 3,
